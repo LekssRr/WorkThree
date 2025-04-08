49bfd936da7fa736f19c363dc8e8b7ba4639d539
Alex <sasha_knyazev_2016@bk.ru>
2025-04-08 21:37:19 +0300

    Create AutoService

--- src/main/java/com/example/Dealer/service/AutoService.java
+++ src/main/java/com/example/Dealer/service/AutoService.java
@@ -1,4 +1,42 @@
 package com.example.Dealer.service;
 
+import com.example.Dealer.dto.AutoDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public class AutoService {
+
+    public AutoDto getServiceCompany(String vinCode)
+    {
+        return null;
+    }
+    public List<AutoDto> getAllAuto()
+    {
+        return null;
+    }
+    public List<AutoDto>getAllAutoToServiceCompany(String nameServiceCompany)
+    {
+        return null;
+    }
+
+    public boolean addAuto(String vinCode, String nameServiceCompany)
+    {
+        return false;
+    }
+
+    public boolean deleteAuto(String vinCode)
+    {
+        return false;
+    }
+    public boolean deleteAllAuto()
+    {
+        return false;
+    }
+
+    public boolean updateAuto()
+    {
+        return false;
+    }
+
 }
--- src/main/java/com/example/Dealer/service/ServiceCompanyService.java
+++ src/main/java/com/example/Dealer/service/ServiceCompanyService.java
@@ -1,4 +1,11 @@
 package com.example.Dealer.service;
 
+import com.example.Dealer.dto.AutoDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
 public class ServiceCompanyService {
+    
 }
