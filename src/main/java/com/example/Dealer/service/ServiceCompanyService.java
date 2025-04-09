3b8e565c923217dd1eb0e64b32957fd20925549e
Alex <sasha_knyazev_2016@bk.ru>
2025-04-09 06:12:52 +0300

    Create ServiceCompanyService

--- src/main/java/com/example/Dealer/service/ServiceCompanyService.java
+++ src/main/java/com/example/Dealer/service/ServiceCompanyService.java
@@ -1,11 +1,39 @@
 package com.example.Dealer.service;
 
 import com.example.Dealer.dto.AutoDto;
+import com.example.Dealer.dto.ServiceCompanyDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public class ServiceCompanyService {
-    
+
+    public List<ServiceCompanyDto> getAllServiceCompany()
+    {
+        return null;
+    }
+    public List<AutoDto> getAllVinToServiceCompany()
+    {
+        return null;
+    }
+
+    public boolean addServiceCompany()
+    {
+        return false;
+    }
+
+    public boolean deleteServiceCompany()
+    {
+        return false;
+    }
+    public boolean deleteAllServiceCompany()
+    {
+        return false;
+    }
+
+    public boolean updateServiceCompany()
+    {
+        return false;
+    }
 }
